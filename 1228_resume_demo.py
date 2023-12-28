323b565abfb83e342086ff30f8a7f105ebffb459
JesusG <py3user22@github.com>
2023-12-28 13:51:55 -0600

    html demo saves 1228

---
+++ 1228_resume_demo.py
@@ -0,0 +1,336 @@
+[6~#!/bin/python3
+
+
+# 1228 resume JesusG.html save
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>122823' resume</title>
+    <style>
+        p { color: blue; border: 2px solid; }
+        a:hover { color: green; background-color: lightBlue; }
+        h1 { color: blue; border: 2px dashed;
+         padding: 0 255px 0 10px; border-radius: 15px 0 15px 0;
+         background-color: lightBlue; margin-right: 250px;
+        }
+        h1:hover { color: green }
+        h3 { color: blue; border: 2px solid blue;
+         padding: 0 15px 0 10px; margin-right: 250px;
+        }
+        h3:hover { color: green }
+    </style>
+</head>
+<body>
+    <h1><i>Jesus Gomez</i></h1>
+    <h3>
+        gmail: <a href="jesusg31.2013@gmail.com">jesusg31.2013@gmail.com</a> <br>
+        github: <a href="https://github.com/py3user22">py3user22</a> <br>
+        youtube: <a href="https://www.youtube.com/@py3pro2023">py3pro2023</a> <br>
+        linkedin: <a href="https://www.linkedin.com/in/jesus-gomez-806332119">jesus-gomez-806332119</a> <br>
+    </h3>
+
+</body>
+</html>
+
+#-------------------------------------------------
+#-------------------------------------------------
+
+
+# 1116 resume demo2.html    save
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>1116_resume demo2</title>
+    <link rel="stylesheet" href="1116_resume2.css">
+</head>
+<body>
+    <h2>
+        Jesus Gomez<br>
+        832-973-1179 (cell)
+    </h2>
+    <h3><sup>Gmail ----|</sup> jesusg31.2013@gmail.com<br>
+        <sup>Linkedin -|</sup> https://www.linkedin.com/in/jesus-gomez-806332119/<br>
+        <sup>Github ---|</sup> https://github.com/py3user22<br>
+        <sup>Youtube --|</sup> https://www.youtube.com/@py3pro2023/<br>
+    </h3>
+
+    <br>
+    <main id="main1">
+
+        <p>
+            <u><b>Introduction</b></u><br>
+            I am available to work full-time, and willing to travel if need be.<br>
+            Anspiring web developer and programmer, I have focused on CyberSecurity for the past year including Networking protocols.<br>
+            I am dedicated to learning new PC skills that will enhance my skillset available.<br>
+            The main focus I enjoy is Python, CySec, & web designing. (resume is a html page that I created)<br>
+
+
+
+
+            <br>
+            <b>Programming languages</b>:<br> Python(main 2yr+), JavaScript, HTML/CSS, Linux bash<br>
+
+            <b>Personal skills</b>:<br> Good communication, Dependable, long-term minded, Handy w/ power tools, <br>
+            & enjoy to help others out in meaningful ways,<br> Always open to new ideas or helpful hints that would increase productivity or time savers.
+            <br>
+
+            <b>Technical skills</b>:<br> CyberSecurity, Cloud Networking, Webpage designer, Cat5e/fiber cabling, <br>
+            Administration, Accounting, Mechanic, Medical equipment installation <br>
+            <b>Future goals</b>:<br> CompTia security+, CompTia network+ certifications. <br>
+        </p>
+        <br>
+
+        <p>
+            <b><u>Education</u></b><br>
+            University of Houston | grad@2014 |>> Accounting (Bachelor's in Science)<br>
+
+            San Jacinto College ....| grad@2010 |>> Business Administration (Associates)
+
+
+        </p>
+
+
+    <br>
+
+
+
+     <div class="container">
+   <div class="row" id="toppg">
+     <td><u>Specialization</u> </td>
+     <td><u>Certificates</u>  </td>
+   </div>
+
+   <br>
+   <div class="row">
+    <div class="col">
+      <td>052322 |</td>
+      <td>IBM Cybersecurity Analyst Professional Certificate //</td>
+    </div>
+   </div>
+
+   <br>
+   <div class="row">
+    <div class="col">
+      <td>051922 |</td>
+      <td>IBM Security Analyst Fundamentals Specialization //</td>
+    </div>
+   </div>
+
+   <br>
+   <div class="row">
+    <div class="col">
+      <td>110722 |</td>
+      <td>Google IT Automation with Python Professional Certificate //</td>
+    </div>
+   </div>
+
+   <br>
+   <div class="row">
+    <div class="col">
+      <td>062523 |</td>
+      <td>Architecting with Google Kubernetes Engine Specialization //</td>
+    </div>
+   </div>
+
+   <br>
+   <div class="row">
+    <div class="col">
+      <td>072223 |</td>
+      <td>Networking in Google Cloud Specialization //</td>
+    </div>
+   </div>
+
+   <br>
+   <div class="row">
+    <div class="col">
+      <td>082723 |</td>
+      <td>Palo Alto Networks Cybersecurity Professional Certificate //</td>
+    </div>
+   </div>
+
+   <br>
+   <div class="row">
+    <div class="col">
+      <td>090923 |</td>
+      <td>Red Hat
+Linux and Private Cloud Administration on IBM Power Systems Specialization //</td>
+    </div>
+   </div>
+
+   <br>
+   <div class="row">
+    <div class="col">
+      <td>091523 |</td>
+      <td>Juniper Networks Security Fundamentals Specialization //</td>
+    </div>
+   </div>
+
+   <br>
+   <div class="row">
+    <div class="col">
+      <td>091923 |</td>
+      <td>ISC<sup>2</sup> Certified in Cybersecurity Specialization //</td>
+    </div>
+   </div>
+
+   <br>
+   <div class="row">
+    <div class="col">
+      <td>101223 |</td>
+      <td>ISC<sup>2</sup> Systems Security Certified Practitioner (SSCP) //</td>
+    </div>
+   </div>
+ </div>
+    <br>
+        <p>
+            <b><u>Work History</u></b><br>
+            U.S. Army | EOS@2010 |>> Mechanic<br>location: Fort Sam Houston, TX., Fort Stewart, GA.<br>
+            Duties: Maintenance, and preventive maintenance on all wheeled vehicles on the base or in field settings.<br>
+            <br>
+            University of Houston | 2011 - 2013 |>> librarian assistant<br>
+            location: library name..<br>
+            Duties: Part-time federal work study program while going to college, opening and closing duties, book re-shelving<br>
+            Full-time use of GI Bill benefits, to pay for expenses, and student loans.<br>
+            <br>
+            Sunbelt Rentals | 2011 - 2013 |>> rental equipment inventory technician<br>
+            location: Oil refinery in La Porte, TX.<br>
+            Duties: Full-time, while working at the library and going to college full-time.<br>Responsible for
+            inventory paperwork of all equipment, help with delivery and pickup of equipment, opening/closing duties of equipment area<br>
+            <br>
+            AT&T | 2013 - 2017 |>> cable installation technician<br>
+            location: Houston, TX. and surrounding areas<br>
+            Duties: Full-time, cable TV, VoIP, internet installer and repairman for residential homes and small business.<br>
+
+            <br>
+            Remetronix | 2017 - 2019 |>> medical equipment installation technician<br>
+            location: Pensecola, FL. but is 100% travel based around the USA.<br>
+            Duties: Full-time, medical equipment installer of a machine made by Siemens called "Atellica"<br>This machine
+            is electro-magnetic belt driven, cutting edge equipment used to sample body fluids for hospitals and laboratories.<br>
+            My duties where get assignment, schedule all travel arrangements, and fly out on Sunday, and finish up before Friday<br>
+            My airlines card had more than 200k miles points on it, so I am well traveled.<br>
+            <br>
+            Unemployed | 2020 - 2021 |>> Student<br>
+            Description: Covid-19 work restrictions and pandemic timeframe<br>
+            Just stayed home and tried to stay safe during this time. <br>
+            <br>
+            Unemployed | 2022 - current |>> Student<br>
+            Descriptions: Started my upskilling journey to become certified in CyberSecurity and IT networking field.<br>
+            I have completed many in the name of knowledge, my GitHub page has a open python dictionary with all certifications.<br>
+            I have used my state and federal work skilling programs to get free access to training to help get into a new job field.<br>
+
+
+
+
+
+
+
+
+        </p>
+
+
+    </main>
+
+    <a href="R:\1004 bootstrap extract files\bootstrap-5.3.2-dist\js\bootstrap.min.js"></a>
+    <br>
+    <br>
+
+
+
+</body>
+</html>
+
+#-------------------------------------------------
+#-------------------------------------------------
+
+
+
+# 1228 final resume save -
+
+mail ----| jesusg31.2013@gmail.com
+Linkedin -| https://www.linkedin.com/in/jesus-gomez-806332119/
+Github ---| https://github.com/py3user22
+
+Introduction
+I am available to work full-time, and available to travel 100%.
+An Aspiring full-stack developer and programmer. I am dedicated to learning new technical skills, and
+in 2023' I have dedicated 1,080+ hours in study time, for IT specialization certifications.
+The main focus I enjoy is Python, CySec, & web designing and development. (resume is a html page
+that I created)
+
+Programming languages:
+Python(main 2yr+), JavaScript, HTML/CSS, Linux bash
+Personal skills:
+Dependable, adaptable, a Handyman w/ power tools.
+Easy going and helpful, focused and ready to work. A team player with good communication skills.
+Technical skills:
+CyberSecurity, Cloud Networking, Webpage designer, Cat5e/fiber cabling, router installation and
+configuration
+Administration, Accounting, Mechanic, Medical equipment installation, drafting, video editing and
+creation youtube.com/@py3pro2023
+Future goals:
+CompTia security+, CompTia network+ certifications.
+
+Education
+University of Houston | grad@2014 |>> Accounting (Bachelor's in Science)
+San Jacinto College ....| grad@2010 |>> Business Administration (Associates)
+
+Specialization Certificates
+052322 | IBM Cybersecurity Analyst Professional Certificate //
+051922 | IBM Security Analyst Fundamentals Specialization //
+110722 | Google IT Automation with Python Professional Certificate //
+062523 | Architecting with Google Kubernetes Engine Specialization //
+072223 | Networking in Google Cloud Specialization //
+082723 | Palo Alto Networks Cybersecurity Professional Certificate //
+090923 | Red Hat Linux and Private Cloud Administration on IBM Power Systems Specialization //
+091523 | Juniper Networks Security Fundamentals Specialization //
+091923 | ISC2 Certified in Cybersecurity Specialization //
+101223 | ISC2 Systems Security Certified Practitioner (SSCP) //
+
+Work History
+
+U.S. Army | EOS@2010 |>> Mechanic
+location: Fort Sam Houston, TX., Fort Stewart, GA.
+Duties: Maintenance, and preventive maintenance on all wheeled vehicles on the base or in field
+settings.
+
+University of Houston | 2011 - 2013 |>> Librarian Assistant
+location: University of Houston -Clearlake
+Duties: Part-time federal work study program while going to college, opening and closing duties, book
+re-shelving
+Full-time use of GI Bill benefits, to pay for expenses, and student loans.
+
+Sunbelt Rentals | 2011 - 2013 |>> Inventory Technician
+location: Dow chemical Oil refinery in La Porte, TX.
+Duties: Full-time, while working at the library and going to college full-time.
+Responsible for inventory paperwork of all equipment, help with delivery and pickup of equipment,
+opening/closing duties of equipment area
+
+AT&T | 2013 - 2017 |>> Installation Technician
+location: Houston, TX. and surrounding areas
+Duties: Full-time, cable TV, VoIP, internet installer and repairman for residential homes and small
+business.
+Router installations using Cat5e cabling, and connection testing and reporting.
+
+Remetronix | 2017 - 2019 |>> Medical Equipment Installation Technician
+location: Pensecola, FL. but is 100% travel based around the USA.
+Duties: Full-time, medical equipment installer of a machine made by Siemens called "Atellica", One of
+about a dozen or so who are certified in this machine.
+My duties where get assignment, schedule travel arrangements, and repeat weekly.
+Get to assigned hospital/laboratory for unpacking and full assembly of machines.
+
+Unemployed | 2020 - 2021 |>> Student
+Description: Covid-19 work restrictions and pandemic timeframe
+Just stayed home and tried to stay safe during this time.
+
+Unemployed | 2022 - current |>> Student
+Descriptions: Started my upskilling journey to become certified in CyberSecurity and IT networking
+field.
+All certifications are saved local and on Github as well as any saves of all completed courses.
+I utilize my state and federal work skilling programs to get access to training to help get into a new job
+field.
+My state employment agency has me scheduled for CompTia courses in Jan.202
+
+
